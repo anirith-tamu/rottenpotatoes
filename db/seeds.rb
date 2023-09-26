2656dd34578e686ba8300521fcb43615b426a07a
anirith-tamu <anirith@tamu.edu>
2023-09-26 16:41:51 -0500

    Added fav movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Avatar', :rating => 'PG-13',
+    :release_date => '18-Dec-2009'},
+  {:title => 'Good Will Hunting', :rating => 'R',
+    :release_date => '05-Dec-1997'},
+  {:title => 'The Dark Knight', :rating => 'PG-13',
+    :release_date => '18-Jul-2008'}
 ]
+
 
 more_movies.each do |movie|
   Movie.create!(movie)
